d821181eaf56067c0da11b3f955b6cca88641e03
arjyo851 <arjyo77@gmail.com>
2022-08-24 16:55:26 +0530

    integrated postgres replaces sqlite and furthur developed application

--- elevator/models.py
+++ elevator/models.py
@@ -4,11 +4,10 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 class Elevator(models.Model):
-    # direction = models.CharField(max_length=4, choices=[('Up', 'Up'), ('Down', 'Down')])
     doorStatus = models.CharField(max_length=6, choices=[('Open', 'Open'), ('Closed', 'Closed')])
     currentFloor = models.IntegerField()
     motion = models.CharField(max_length=7, choices=[('Moving', 'Moving'), ('Stopped', 'Stopped')])
     operational = models.BooleanField(default=True)
-    listofRequest = ArrayField(models.IntegerField(blank=True), default=list)
+    listofRequest = ArrayField(models.IntegerField(default=list, blank=True,null=True), blank=True,null=True)
 
 
--- elevator/urls.py
+++ elevator/urls.py
@@ -8,14 +8,11 @@ urlpatterns = [
     path('direction/<int:pk>', views.direction, name='direction'), #Get Direction Up or down?
     path('status/<int:pk>', views.status, name='status'),#Get and Post(maintanence or operational)
     path('door/<int:pk>', views.door, name='door'),#Get and Post door status manually(open or closed)
-    path('liftAsssigner', views.liftAssigner, name='optimal'),#Get Optimal Elevator id
+    path('liftAsssigner', views.liftAssigner, name='optimal'),#Post Optimal Elevator id
     path('active/<int:pk>', views.active, name='active'),#Get busy/Active Elevator id by motion
     path('motion/<int:pk>', views.motion, name='motion'),#Get and Post Status of Motion (moving or stopped)
+    path('runLift/<int:pk>', views.runLift, name='runLift'),# run the lift acc to listofRequest
     path('deinitialise', views.deinitialise, name='deinitialise'),
 
 
 ]
-# currentfloor does not update : needs a api which can update currentfloor
-#deinitialise : delete all objects from database
-#listofrequest: send only a number and append it in list
-#run list of request
